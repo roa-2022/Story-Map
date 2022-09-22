ebbd9854355898f197ef4a1adc444dab76d46bd8
Amy He <amyhe1995@gmail.com>
2022-09-22 17:12:42 +1200

    finish get one story db function

--- server/routes/stories.js
+++ server/routes/stories.js
@@ -4,9 +4,9 @@ const db = require('../db/stories')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  // const id = req.params.id
-  db.getStories()
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  db.getOneStory(id)
     .then((result) => {
       console.log(result)
       res.json(result)
@@ -15,14 +15,6 @@ router.get('/', (req, res) => {
       console.log(err)
       res.status(500).json({ message: 'Something went wrong' })
     })
-  // db.getOneStory(id)
-  //   .then((result) => {
-  //     res.json(result)
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //     res.status(500).json({ message: 'Something went wrong' })
-  //   })
 })
 
 module.exports = router
