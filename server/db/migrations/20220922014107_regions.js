a01c96548f419f87c1099d47c50e1326f9554c27
Amy He <amyhe1995@gmail.com>
2022-09-22 13:50:54 +1200

    set up migration

---
+++ server/db/migrations/20220922014107_regions.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('regions', (table) => {
+    table.increments('id')
+    table.string('eng_name')
+    table.string('maori_name')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('regions')
+}
---
+++ server/db/migrations/20220922014154_savedStories.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('savedStories', (table) => {
+    table.increments('id')
+    table.string('story_id')
+    table.string('user_id')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('savedStories')
+}
