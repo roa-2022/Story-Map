2b265f9995d5aa7d5abb03386da11ed616fdc632
Yanina <99714877+yanina-amatore@users.noreply.github.com>
2022-09-23 09:43:30 +1200

    commit to pull

---
+++ client/actions/addStory.js
@@ -0,0 +1,20 @@
+import { 
+  addStoryAPI
+} from '../apis/addStory'
+
+export const ADD_STORY ='ADD_STORY'
+
+// ---- ADD STORY
+export function addStory(data){
+  return {
+    type:ADD_STORY ,
+    payload: data
+  }
+}
+
+//Thunk
+export function fetchAddStory(data) {
+  return (dispatch) => {
+    return addStoryApi(data).then((res) => dispatch(addStory(res)))
+  }
+}
--- client/components/AddStory.tsx
+++ client/components/AddStory.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-function AddStory() {
-  const [dataForm, setDataForm] = useState(null)
 
+
+function AddStory() {
   const dispatch = useDispatch()
+
+  const [dataForm, setDataForm] = useState('')
+
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -13,68 +17,75 @@ function AddStory() {
   }
 
   const handleChange = (e) => {
-    // setDataForm({
-    //   ...dataForm,
-    //   [e.target.name]: e.target.value,
-    // })
+    setDataForm({
+      ...dataForm,
+      [e.target.name]: e.target.value,
+    })
   }
   return (
-  
-      <section className="add-container">
-        <div className="add-box"></div>
-        <div className="add-box">
-          <div className="add-box-text">
-            <h2>Add your Story</h2>
-          </div>
-          <div className="form-container">
-            <form className="grid-stacked" onSubmit={handleSubmit}>
-              <div className="input-group">
-                <label htmlFor="type"> Type of Venue </label>
-                <select name="type" id="type" onChange={handleChange}>
-                  <option value="DEFAULT">Select an Option</option>
-                  <option defaultValue="">New Zealand</option>
-                  <option value="">North Island</option>
-                  <option value="">South Island</option>
-                  <option value="">Northland</option>
-                  <option value="">Auckland</option>
-                  <option value="">Waikato</option>
-                  <option value="">Bay of Plenty</option>
-                  <option value="">Gisborne</option>
-                  <option value="">Hawke's Bay</option>
-                  <option value="">Taranaki</option>
-                  <option value="">Manawatū-Whanganui</option>
-                  <option value="">Wellington</option>
-                  <option value="">Tasman</option>
-                  <option value="">Nelson</option>
-                  <option value="">Marlborough</option>
-                  <option value="">West Coast</option>
-                  <option value="">Canterbury</option>
-                  <option value="">Otago</option>
-                  <option value="">Southland</option>
-                </select>
-              </div>
-              <div className="input-group">
-                <label htmlFor="author">Author: </label>
-                <input type="text" name="author" onChange={handleChange} />
-              </div>
-              <div>
-                <label htmlFor="title">Title: </label>
-                <input type="text" name="title" onChange={handleChange} />
-              </div>
-              <div>
-                <label htmlFor="synopsis">Website link: </label>
-                <input type="text" name="synopsis" onChange={handleChange} />
-              </div>
-              <div>
-                
-              </div>
-              <div>
-                <button className="btn-add-venue">Add</button>
-              </div>
-            </form>
-          </div>
+    <section className="add-container">
+      <div className="add-box"></div>
+      <div className="add-box">
+        <div className="add-box-text">
+          <h2>Add your Story</h2>
         </div>
-      </section>
+        <div className="form-container">
+          <form className="grid-stacked" onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="type">region </label>
+              <select name="type" id="type" onChange={handleChange}>
+                <option value="DEFAULT">Select an Option</option>
+                <option defaultValue="">New Zealand</option>
+                <option value="">North Island</option>
+                <option value="">South Island</option>
+                <option value="">Northland</option>
+                <option value="">Auckland</option>
+                <option value="">Waikato</option>
+                <option value="">Bay of Plenty</option>
+                <option value="">Gisborne</option>
+                <option value="">Hawke's Bay</option>
+                <option value="">Taranaki</option>
+                <option value="">Manawatū-Whanganui</option>
+                <option value="">Wellington</option>
+                <option value="">Tasman</option>
+                <option value="">Nelson</option>
+                <option value="">Marlborough</option>
+                <option value="">West Coast</option>
+                <option value="">Canterbury</option>
+                <option value="">Otago</option>
+                <option value="">Southland</option>
+              </select>
+            </div>
+            <div className="input-group">
+              <label htmlFor="author">Author: </label>
+              <input type="text" name="author" onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="title">Title: </label>
+              <input type="text" name="title" onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="synopsis">Synopsis: </label>
+              <input type="text" name="synopsis" onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="story_text">Type your story: </label>
+              <br/>
+              <textarea
+                id="story_text"
+                onChange={handleChange}
+                placeholder="Write your story here"             
+                rows={10}
+                cols={50}             
+              />
+            </div>
+            <div>
+              <button className="btn-add-venue">Add</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
