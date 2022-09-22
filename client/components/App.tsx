de87d3b7d97aec67f085ef12547e2e57bba0eead
Amy He <amyhe1995@gmail.com>
2022-09-23 11:43:56 +1200

    set up login reducer api

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import Home from './Home'
@@ -10,11 +10,28 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { clearLoggedInUser, updateLoggedInUser } from '../actions/loggedInUser'
 import { useCacheUser } from '../auth0-utils'
 
+import { getUser } from '../apis/users'
+
 function App() {
   useCacheUser()
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const { isAuthenticated, getAccessTokenSilently } = useAuth0()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      dispatch(clearLoggedInUser())
+    } else {
+      getAccessTokenSilently()
+        .then((token) => getUser(token))
+        .then((userInDb) => {
+          userInDb
+            ? dispatch(updateLoggedInUser(userInDb))
+            : navigate('/register')
+        })
+        .catch((err) => console.error(err))
+    }
+  }, [isAuthenticated])
   return (
     <>
       <header className="header">
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import search from './search'
 import stories from './stories'
+import user from './loggedInUser'
 
 export default combineReducers({
+  user,
   stories,
-  search
+  search,
 })
