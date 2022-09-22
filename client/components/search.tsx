587d0d124e5283aebedc7ed3c61877c6a77b44dc
cornrowsMatt <mtuson1414@gmail.com>
2022-09-23 09:39:17 +1200

    image urls created but not viewable

--- client/components/search.tsx
+++ client/components/search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchSearchedRegions, fetchMaoriSearchedRegions } from '../actions/search'
 
@@ -7,45 +7,52 @@ export default function search() {
     const [region, setRegion ] = useState('') 
     const [maoriRegion, setMaoriRegion ] = useState('') 
     const dispatch = useDispatch()
-    const search = useSelector((state: any) => state.search)
-    const [file, setFile] = useState(null)
+    const search = useSelector((state:any) => state.search)
+
+    const [files, setFiles]:any = useState([])
+    const [imageURLs, setImageURLs]: any = useState([])
+
+    useEffect(() => {
+        if(files.length < 1) return;
+        const newImageUrls:any = []
+        files.forEach(file => newImageUrls.push(URL.createObjectURL(file)))
+        setImageURLs(newImageUrls)
+    }, [files])
     const fileSelector = (e) => {
         console.log(e.target.files[0]);
-        setFile(e.target.files[0])
-        console.log(file);
-    }
-
-    const submitHandler = () => {
-        dispatch(fetchSearchedRegions(region))
+        setFiles([...e.target.files])
     }
     
     const fileUploader = () => {
-
+        console.log(files);
+        console.log(imageURLs);
     }
 
   return (
     <>
-      <div>
-        <label>Filter By Region(English)
-        <input onChange = {(e) => {setRegion(e.target.value)}}/>
-        </label>
-        <button onClick={() => submitHandler()}>Click</button>
-        {search?.map(story => {
-            return <h3>{story?.story_id}</h3>
-            
-        })}
-    </div>
-         <div>
-         <label>Filter By Region(Maori)
-         <input onChange = {(e) => {setMaoriRegion(e.target.value)}}/>
-         </label>
-         <button onClick={() => dispatch(fetchMaoriSearchedRegions(maoriRegion))}>Click</button>
-         <h3>{search?.region_id}</h3>
-     </div>
-     <input type= "file" onChange = {fileSelector} id = "img" accept= "image/png, image/jpg" ></input>
-        <button onClick={fileUploader}>Upload</button>
-    <div>
-    </div>
+        <div>
+            <label>Filter By Region(English)
+            <input onChange = {(e) => {setRegion(e.target.value)}}/>
+            </label>
+            <button onClick={() => dispatch(fetchSearchedRegions(region))}>Click</button>
+            {search?.map(story => {
+                return <h3>{story?.story_id}</h3>
+                
+            })}
+        </div>
+        <div>
+            <label>Filter By Region(Maori)
+            <input onChange = {(e) => {setMaoriRegion(e.target.value)}}/>
+            </label>
+            <button onClick={() => dispatch(fetchMaoriSearchedRegions(maoriRegion))}>Click</button>
+            <h3>{search?.region_id}</h3>
+        </div>
+
+        <div>
+            <input type= "file" onChange = {fileSelector} id = "img" accept= "image/*" ></input>
+            <button onClick={fileUploader}>Upload</button>
+        </div>
+    
     </>
   )
 }
