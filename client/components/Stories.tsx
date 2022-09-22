6c14d8abf07f6fd226cc05fb82029bd62bb6c16b
Tyrone Elia <tyroneelia@gmail.com>
2022-09-23 09:36:42 +1200

    merge conflict resos

--- client/components/Stories.tsx
+++ client/components/Stories.tsx
@@ -1,20 +1,15 @@
 import React from 'react'
 
-<<<<<<< HEAD
-import stories from '../components/Stories'
 
 function Stories() {
   return 
   <>
   <div>
     <h3>Stories</h3>
-    
+    <div>
+
+    </div>
   </div>
   </>
-=======
-function Stories() {
-  return <></>
->>>>>>> 0770e9a0dc9afc9623846df977b965ea321019cf
-}
 
 export default Stories
--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-<<<<<<< HEAD
-=======
 const searchRoutes = require('./routes/search')
->>>>>>> 0770e9a0dc9afc9623846df977b965ea321019cf
 const storiesRoutes = require('./routes/stories')
 
 const server = express()
@@ -12,10 +9,7 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-<<<<<<< HEAD
-=======
 server.use('/api/search', searchRoutes)
->>>>>>> 0770e9a0dc9afc9623846df977b965ea321019cf
 server.use('/api/v1/stories', storiesRoutes)
 // to go to front end routes
 server.get('*', (req, res) => {
