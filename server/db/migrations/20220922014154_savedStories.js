649fb19cb210bcc4afe365631444d2cf7a707f52
Amy He <amyhe1995@gmail.com>
2022-09-22 16:17:13 +1200

    update seed

--- server/db/migrations/20220922014154_savedStories.js
+++ server/db/migrations/20220922014154_savedStories.js
@@ -1,12 +1,10 @@
-
 exports.up = function (knex) {
   return knex.schema.createTable('saved_stories', (table) => {
     table.increments('id')
-    table.string('story_id')
-    table.string('user_id')
+    table.string('story_id').references('stories.id')
+    table.string('user_id').references('users.id')
   })
 }
-
 
 exports.down = function (knex) {
   return knex.schema.dropTable('saved_stories')
