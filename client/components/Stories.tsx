c554fbb5f8782038e49a973dc2f0d1f8f651bd76
Amy He <amyhe1995@gmail.com>
2022-09-23 14:31:51 +1200

    add button link"

--- client/components/Stories.tsx
+++ client/components/Stories.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchStories } from '../actions/index'
+import { Link } from 'react-router-dom'
 
 import Search from './Search'
-
-
 
 function Stories() {
   const dispatch = useDispatch()
@@ -18,15 +17,21 @@ function Stories() {
       <Search />
       <div>
         <h3>Whenua O Korero</h3>
-          {stories.map((story) => {
-          return <div key={story.id}>
-            <p>{story?.title}</p>
-            <p>{story?.location}</p>
-            <p>{story?.synopsis}</p>
+        {stories.map((story) => {
+          return (
+            <div key={story.id}>
+              <p>{story?.title}</p>
+              <p>{story?.location}</p>
+              <p>{story?.synopsis}</p>
+              <Link to={`/stories/${story.id}`}>
+                <button>More Infor</button>
+              </Link>
             </div>
+          )
         })}
       </div>
     </>
-  )}
+  )
+}
 
 export default Stories
