3dba734440eeb660de0cbf562ecd95ab1ab66ae0
Amy He <amyhe1995@gmail.com>
2022-09-26 11:30:48 +1300

    finish update function

---
+++ client/components/UpdateStory.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { updateStory } from '../apis/story'
+
+function UpdateStory() {
+  const navigate = useNavigate()
+  const token = useSelector((store: any) => store.user.token)
+  const allRegions = useSelector((store: any) => store.regions)
+  const stories = useSelector((store: any) => store.stories)
+  const story = stories[0]
+  const [dataForm, setDataForm] = useState(story)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log('token', token)
+    console.log('dataForm', dataForm)
+    updateStory(dataForm, token)
+    navigate('/stories')
+  }
+  const handleChange = (e) => {
+    setDataForm({
+      ...dataForm,
+      [e.target.name]: e.target.value,
+    })
+  }
+  return (
+    <section className="add-container">
+      <div className="add-box"></div>
+      <div className="add-box">
+        <div className="add-box-text">
+          <h2>Update your Story</h2>
+        </div>
+        {story && (
+          <div className="form-container">
+            <form className="grid-stacked" onSubmit={handleSubmit}>
+              <div className="input-group">
+                <label htmlFor="region_id">region </label>
+                <select name="region_id" id="type" onChange={handleChange}>
+                  <option value="DEFAULT">Select an Option</option>
+                  <option value={allRegions[0]?.id}>
+                    New Zealand | Aotearoa
+                  </option>
+                  <option value={allRegions[1]?.id}>
+                    North Island | Te Ika-a-Māui
+                  </option>
+                  <option value={allRegions[2]?.id}>
+                    South Island | Te Waipounamu
+                  </option>
+                  <option value={allRegions[3]?.id}>
+                    Northland | Te Tai Tokerau{' '}
+                  </option>
+                  <option value={allRegions[4]?.id}>Auckland</option>
+                  <option value={allRegions[5]?.id}>Waikato</option>
+                  <option value={allRegions[6]?.id}>Bay of Plenty</option>
+                  <option value={allRegions[7]?.id}>Gisborne</option>
+                  <option value={allRegions[8]?.id}>Hawke's Bay</option>
+                  <option value={allRegions[9]?.id}>Taranaki</option>
+                  <option value={allRegions[10]?.id}>Manawatū-Whanganui</option>
+                  <option value={allRegions[11]?.id}>Wellington</option>
+                  <option value={allRegions[12]?.id}>Tasman</option>
+                  <option value={allRegions[13]?.id}>Nelson</option>
+                  <option value={allRegions[14]?.id}>Marlborough</option>
+                  <option value={allRegions[15]?.id}>West Coast</option>
+                  <option value={allRegions[16]?.id}>Canterbury</option>
+                  <option value={allRegions[17]?.id}>Otago</option>
+                  <option value={allRegions[18]?.id}>Southland</option>
+                </select>
+              </div>
+              <div className="input-group">
+                <label htmlFor="author">Author: </label>
+                <input
+                  type="text"
+                  name="author"
+                  defaultValue={story.author}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="title">Title: </label>
+                <input
+                  type="text"
+                  name="title"
+                  defaultValue={story.title}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="synopsis">Synopsis: </label>
+                <input
+                  type="text"
+                  name="synopsis"
+                  defaultValue={story.synopsis}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="story_text">Type your story: </label>
+                <br />
+                <textarea
+                  name="story_text"
+                  defaultValue={story.story_text}
+                  onChange={handleChange}
+                  placeholder="Write your story here"
+                  rows={10}
+                  cols={50}
+                />
+              </div>
+              <div>
+                <button className="btn-add-venue">Update</button>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
+    </section>
+  )
+}
+
+export default UpdateStory
--- server/db/seeds/stories.js
+++ server/db/seeds/stories.js
@@ -13,11 +13,11 @@ exports.seed = async function (knex) {
         'The summit where Tamatea, the man with the big knees, the slider, climber of mountains, the land-swallower who travelled about, played his koauau to his loved one - "The summit where Tamatea, the man with the big knees, the slider, climber of mountains, the land-swallower who travelled about, played his koauau to his loved one.',
       photo_url: '/images/img01.png',
       longitude: 176.5498497962975,
-      latitude: -40.3493780343085
+      latitude: -40.3493780343085,
     },
     {
       id: 1001,
-      auth0_id: 'auth0|632d259b6aa7af87dfa37429',
+      auth0_id: 'auth0|632cf8dd563ea914bb89ec3a',
       author: 'Whenua O Korero',
       title: 'Tāne Mahuta',
       synopsis:
@@ -26,7 +26,7 @@ exports.seed = async function (knex) {
         'Tāne Mahuta, Lord of the Forest, is an important figure in Māori history. The oldest of six siblings, Tāne Mahuta, grew tired of living in darkness, closed in between his sky father (Ranginui) and earth mother (Papa-tū-ā-nuku). He decided to push them apart and, in doing so, created the world of light (Te Ao Mārama) we live in today. - Tāne Mahuta, Lord of the Forest, is an important figure in Māori history. The oldest of six siblings, Tāne Mahuta, grew tired of living in darkness, closed in between his sky father (Ranginui) and earth mother (Papa-tū-ā-nuku). He decided to push them apart and, in doing so, created the world of light (Te Ao Mārama) we live in today.',
       photo_url: '/images/img02.jpeg',
       longitude: 173.52741271904605,
-      latitude: -35.600178814634624
+      latitude: -35.600178814634624,
     },
     {
       id: 1002,
@@ -39,7 +39,7 @@ exports.seed = async function (knex) {
         'Hinemoa and Tutanekai were New Zealand’s very own Romeo and Juliet – two star-crossed lovers whose liaison was both passionate and forbidden. This tale has a happy ending though, as they proved the strength of their love through a dramatic and dangerous act, thus gaining the acceptance of their families.Hinemoa and Tutanekai were New Zealand’s very own Romeo and Juliet – two star-crossed lovers whose liaison was both passionate and forbidden. This tale has a happy ending though, as they proved the strength of their love through a dramatic and dangerous act, thus gaining the acceptance of their families.',
       photo_url: '/images/img03.jpeg',
       longitude: 176.25148577790202,
-      latitude: -38.137655667084815
+      latitude: -38.137655667084815,
     },
     {
       id: 1003,
@@ -52,7 +52,7 @@ exports.seed = async function (knex) {
         'The first explorer to discover Aotearoa, New Zealand, was the intrepid ancestor, Kupe. Using the stars and ocean currents as his navigational guides, he ventured across the Pacific on his waka hourua (voyaging canoe) from his ancestral Polynesian homeland of Hawaiki. Kupe`s waka first landed on the shores of the Hokianga Harbour in Northland around 1000 years ago.The first explorer to discover Aotearoa, New Zealand, was the intrepid ancestor, Kupe. Using the stars and ocean currents as his navigational guides, he ventured across the Pacific on his waka hourua (voyaging canoe) from his ancestral Polynesian homeland of Hawaiki. Kupe`s waka first landed on the shores of the Hokianga Harbour in Northland around 1000 years ago.',
       photo_url: '/images/img04.jpeg',
       longitude: 173.39324189846081,
-      latitude: -35.505450113296575
+      latitude: -35.505450113296575,
     },
     // {
     //   id: 1004,
@@ -78,7 +78,7 @@ exports.seed = async function (knex) {
         'Māori legend tells the story of the ancestor Paikea who journeyed to a new life in Aotearoa, New Zealand, on the back of the Tohorā whale. The story represents the spiritual bond between humans and the natural world and the potential revealed when nature is respected rather than exploited.Māori legend tells the story of the ancestor Paikea who journeyed to a new life in Aotearoa, New Zealand, on the back of the Tohorā whale. The story represents the spiritual bond between humans and the natural world and the potential revealed when nature is respected rather than exploited.',
       photo_url: '/images/img06.jpg',
       longitude: 170.14202433438962,
-      latitude: -43.59432165218203
+      latitude: -43.59432165218203,
     },
     // {
     //   id: 1006,
