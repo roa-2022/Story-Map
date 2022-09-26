b96a32f5c6321e92ff1c344893579949ad7f8a4e
Amy He <amyhe1995@gmail.com>
2022-09-27 10:30:51 +1300

    change database migration

--- server/db/migrations/z20220922014154_savedStories.js
+++ server/db/migrations/z20220922014154_savedStories.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('saved_stories', (table) => {
     table.increments('id')
     table.integer('story_id').references('stories.id')
-    table.integer('user_id').references('users.auth0_id')
+    table.integer('user_id')
   })
 }
 
