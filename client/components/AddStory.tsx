9fc13fb3d208cf33b16296c28a249ddc233b440d
Matthew Egan <69609987+mattegan111@users.noreply.github.com>
2022-09-26 13:59:13 +1300

    Revert "Add Story form now has lat/lng coordinates"
    This reverts commit 0a14dc15a00650639ee630df26119e85b8f30662.

--- client/components/AddStory.tsx
+++ client/components/AddStory.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-import Map from './Map'
 
 import { fetchAddStory } from '../actions/addStory'
 
@@ -14,10 +13,7 @@ function AddStory() {
   const allRegions = useSelector((store: any) => store.regions)
   const viewCoordinates = useSelector((state: any) => state.map)
   
-  const [dataForm, setDataForm] = useState({
-    latitude: viewCoordinates.latitude,
-    longitude: viewCoordinates.longitude,
-  })
+  const [dataForm, setDataForm] = useState({})
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -33,78 +29,69 @@ function AddStory() {
   }
 
   return (
-    <>
-      <section className="add-container">
-        <div className="add-box"></div>
-        <div className="add-box">
-          <div className="add-box-text">
-            <h2>Add your Story</h2>
-          </div>
-          <div className="form-container">
-            <form className="grid-stacked" onSubmit={handleSubmit}>
-              <div className="input-group">
-                <label htmlFor="region_id">region </label>
-                <select name="region_id" id="type" onChange={handleChange}>
-                  <option value="DEFAULT">Select an Option</option>
-                  <option value={allRegions[0]?.id}>New Zealand | Aotearoa</option>
-                  <option value={allRegions[1]?.id}>North Island | Te Ika-a-Māui</option>
-                  <option value={allRegions[2]?.id}>South Island | Te Waipounamu</option>
-                  <option value={allRegions[3]?.id}>Northland | Te Tai Tokerau </option>
-                  <option value={allRegions[4]?.id}>Auckland | </option>
-                  <option value={allRegions[5]?.id}>Waikato</option>
-                  <option value={allRegions[6]?.id}>Bay of Plenty</option>
-                  <option value={allRegions[7]?.id}>Gisborne</option>
-                  <option value={allRegions[8]?.id}>Hawke's Bay</option>
-                  <option value={allRegions[9]?.id}>Taranaki</option>
-                  <option value={allRegions[10]?.id}>Manawatū-Whanganui</option>
-                  <option value={allRegions[11]?.id}>Wellington</option>
-                  <option value={allRegions[12]?.id}>Tasman</option>
-                  <option value={allRegions[13]?.id}>Nelson</option>
-                  <option value={allRegions[14]?.id}>Marlborough</option>
-                  <option value={allRegions[15]?.id}>West Coast</option>
-                  <option value={allRegions[16]?.id}>Canterbury</option>
-                  <option value={allRegions[17]?.id}>Otago</option>
-                  <option value={allRegions[18]?.id}>Southland</option>
-                </select>
-              </div>
-              <div className="input-group">
-                <label htmlFor="author">Author: </label>
-                <input type="text" name="author" onChange={handleChange} />
-              </div>
-              <div>
-                <label htmlFor="title">Title: </label>
-                <input type="text" name="title" onChange={handleChange} />
-              </div>
-              <div>
-                <label htmlFor="synopsis">Synopsis: </label>
-                <input type="text" name="synopsis" onChange={handleChange} />
-              </div>
-              <div>
-                <label htmlFor="story_text">Type your story: </label>
-                <br />
-                <textarea
-                  name="story_text"
-                  onChange={handleChange}
-                  placeholder="Write your story here"
-                  rows={10}
-                  cols={50}
-                />
-              </div>
-              <div>
-                <label htmlFor="latitude">Latitude: </label>
-                <input type="text" name="latitude" value={viewCoordinates.latitude} readOnly />
-                <label htmlFor="longitude">Longitude: </label>
-                <input type="text" name="longitude" value={viewCoordinates.longitude} readOnly />
-              </div>
-              <div>
-                <button className="btn-add-venue">Add</button>
-              </div>
-            </form>
-          </div>
+    <section className="add-container">
+      <div className="add-box"></div>
+      <div className="add-box">
+        <div className="add-box-text">
+          <h2>Add your Story</h2>
         </div>
-      </section>
-      <Map />
-    </>
+        <div className="form-container">
+          <form className="grid-stacked" onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="region_id">region </label>
+              <select name="region_id" id="type" onChange={handleChange}>
+                <option value="DEFAULT">Select an Option</option>
+                <option value={allRegions[0]?.id}>New Zealand | Aotearoa</option>
+                <option value={allRegions[1]?.id}>North Island | Te Ika-a-Māui</option>
+                <option value={allRegions[2]?.id}>South Island | Te Waipounamu</option>
+                <option value={allRegions[3]?.id}>Northland | Te Tai Tokerau </option>
+                <option value={allRegions[4]?.id}>Auckland | </option>
+                <option value={allRegions[5]?.id}>Waikato</option>
+                <option value={allRegions[6]?.id}>Bay of Plenty</option>
+                <option value={allRegions[7]?.id}>Gisborne</option>
+                <option value={allRegions[8]?.id}>Hawke's Bay</option>
+                <option value={allRegions[9]?.id}>Taranaki</option>
+                <option value={allRegions[10]?.id}>Manawatū-Whanganui</option>
+                <option value={allRegions[11]?.id}>Wellington</option>
+                <option value={allRegions[12]?.id}>Tasman</option>
+                <option value={allRegions[13]?.id}>Nelson</option>
+                <option value={allRegions[14]?.id}>Marlborough</option>
+                <option value={allRegions[15]?.id}>West Coast</option>
+                <option value={allRegions[16]?.id}>Canterbury</option>
+                <option value={allRegions[17]?.id}>Otago</option>
+                <option value={allRegions[18]?.id}>Southland</option>
+              </select>
+            </div>
+            <div className="input-group">
+              <label htmlFor="author">Author: </label>
+              <input type="text" name="author" onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="title">Title: </label>
+              <input type="text" name="title" onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="synopsis">Synopsis: </label>
+              <input type="text" name="synopsis" onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="story_text">Type your story: </label>
+              <br />
+              <textarea
+                name="story_text"
+                onChange={handleChange}
+                placeholder="Write your story here"
+                rows={10}
+                cols={50}
+              />
+            </div>
+            <div>
+              <button className="btn-add-venue">Add</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
