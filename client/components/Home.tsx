64f0d591691a61d4790ae766f03f0a7871805364
Yanina <99714877+yanina-amatore@users.noreply.github.com>
2022-09-26 19:27:11 +1300

    merged  home changes

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -4,36 +4,57 @@ import Nav from './Nav'
 import { Link } from 'react-router-dom'
 
 function Home() {
-  const externalImage = 'https://picsum.photos/id/128'
+  
   return (
     <>
-      <section className="hero  home-hero-img is-fullheight">
+      <section className="hero-1 is-fullheight">
         <div className="hero-body">
           <div className="container ">
-            <div className="columns is-vcentered">
-              <div className="column is-6">{/* fist column  */}</div>
-              <div className="column"></div>
-              <div className="container is-flex-direction-column home-txt-btn">
-                <div className=" box bd-notification is-primary">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Libero suscipit nihil quo, a magnam enim itaque cumque odio
-                    exercitationem ducimus beatae aperiam modi nesciunt debitis
-                    quaerat iusto culpa incidunt tempora, doloribus saepe
-                    expedita dolorem accusantium, harum earum. Sequi architecto,
-                    sapiente ut dolorem consequatur in culpa, eveniet dolorum
-                    maxime porro accusamus?
+            <div className="columns ">
+              <div className="column is-6">
+                <div className="container is-flex-direction-column home-txt-btn">
+                  <p className=" is-size-1-desktop has-text-light my-6">
+                    Whenua O Korero
                   </p>
-                </div>
-                <div>
-                  <Link to="/stories">
-                    <button className="button is-primary">
-                      Check Get Stories
-                    </button>
-                  </Link>
+                  <div className=" box bd-notification has-background-light ">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Libero suscipit nihil quo, a magnam enim itaque cumque
+                      odio exercitationem ducimus beatae aperiam modi nesciunt
+                      debitis quaerat iusto culpa incidunt tempora, doloribus
+                      saepe expedita dolorem accusantium, harum earum. Sequi
+                      architecto, sapiente ut dolorem consequatur in culpa,
+                      eveniet dolorum maxime porro accusamus?
+                    </p>
+                  </div>
+                  <div>
+                    <Link to="/stories">
+                      <button className="button is-large is-primary">
+                        Stories
+                        <i className="fa-sharp fa-solid fa-arrow-right mx-2"></i>
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="hero is-small has-background-light">
+        <div className="hero-body is-flex is-justify-content-center">
+          <div className="content has-text-centered">
+            <p>
+              <strong>Whenua O Korero</strong> by{' '}
+              <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
+              licensed
+              <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+              The website content is licensed{' '}
+              <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+                CC BY NC SA 4.0
+              </a>
+              .
+            </p>
           </div>
         </div>
       </section>
