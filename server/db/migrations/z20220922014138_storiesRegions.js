6722a9c4579a0509d0a8bfe89918ae874f99da12
Amy He <amyhe1995@gmail.com>
2022-09-27 14:27:19 +1300

    reset deployment database

--- server/db/migrations/z20220922014138_storiesRegions.js
+++ server/db/migrations/z20220922014138_storiesRegions.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('storiesregions', (table) => {
+  return knex.schema.createTable('storiesRegions', (table) => {
     table.increments('id')
     table.integer('story_id')
     table.integer('region_id')
