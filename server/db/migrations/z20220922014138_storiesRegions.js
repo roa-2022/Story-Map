257c9677305ef049bc6977cfec448ae7f1d44ae0
Amy He <amyhe1995@gmail.com>
2022-09-27 14:41:00 +1300

    update seed"

--- server/db/migrations/z20220922014138_storiesRegions.js
+++ server/db/migrations/z20220922014138_storiesRegions.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('storiesregions', (table) => {
+  return knex.schema.createTable('storiesRegions', (table) => {
     table.increments('id')
     table.integer('story_id')
     table.integer('region_id')
@@ -7,5 +7,5 @@ exports.up = function (knex) {
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTable('storiesregions')
+  return knex.schema.dropTable('storiesRegions')
 }
