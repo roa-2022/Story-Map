278d78005f217c1239ca77045e5ca4e2b9019798
Amy He <amyhe1995@gmail.com>
2022-09-27 15:22:08 +1300

    reset database

--- server/db/stories.js
+++ server/db/stories.js
@@ -2,15 +2,15 @@ const connection = require('./connection')
 
 function getStories(db = connection) {
   return db('regions')
-    .join('storiesRegions', 'regions.id', 'storiesRegions.region_id')
-    .join('stories', 'storiesRegions.story_id', 'stories.id')
+    .join('stories_regions', 'regions.id', 'stories_regions.region_id')
+    .join('stories', 'stories_regions.story_id', 'stories.id')
     .select('*')
 }
 
 function getOneStory(id, db = connection) {
   return db('regions')
-    .join('storiesRegions', 'regions.id', 'storiesRegions.region_id')
-    .join('stories', 'storiesRegions.story_id', 'stories.id')
+    .join('stories_regions', 'regions.id', 'stories_regions.region_id')
+    .join('stories', 'stories_regions.story_id', 'stories.id')
     .select('*')
     .where('stories.id', id)
 }
@@ -23,7 +23,7 @@ function addStory(newStoryData, db = connection) {
 
 function addStoryRegions(idObj, db = connection) {
   console.log('idObj', idObj)
-  return db('storiesRegions').insert(idObj)
+  return db('stories_regions').insert(idObj)
 }
 
 //only users can del story by themsevles
