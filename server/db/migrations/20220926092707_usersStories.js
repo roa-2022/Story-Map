4c3afb4cd19264019897d98808523a9ae106cb47
Amy He <amyhe1995@gmail.com>
2022-09-28 13:33:41 +1300

    remove extra table

--- server/db/migrations/20220926092707_usersStories.js
+++ server/db/migrations/20220926092707_usersStories.js
@@ -1,14 +1,11 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('usersStories', (table) => {
-      table.increments('id')
-      table.integer('story_id')
-      table.integer('auth0_id')
-    })
-  }
-  
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTable('usersStories')
-  }
-  
-  
+  return knex.schema.createTable('usersStories', (table) => {
+    table.increments('id')
+    table.integer('story_id')
+    table.string('auth0_id')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('usersStories')
+}
